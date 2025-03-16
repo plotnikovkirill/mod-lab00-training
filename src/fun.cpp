bbce754dd8dabe2a7b7360d4c2686f7ae418afbf
plotnikovkirill <82108541+plotnikovkirill@users.noreply.github.com>
2025-03-16 20:00:04 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -3,8 +3,7 @@
 
 int64_t power(int64_t x, uint16_t n) {
     int64_t prz = 1;
-    for(int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         prz = prz * x;
     }
     return prz;
